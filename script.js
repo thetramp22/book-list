d323c4d8c2461fec5ca13feab3ae1013d671251d
thetramp22 <scottstarks.games@gmail.com>
2023-09-15 21:48:47 -0700

    working on book card buttons

--- script.js
+++ script.js
@@ -6,19 +6,53 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+Book.prototype.toggleRead = function() {
+    this.read = !this.read;
+}
+
 function addBookToList(book) {
     myBookList.push(book);
 }
 
 const myBookList = [{title : 'book', author : 'writer', pages : 234, read : false}, {title : 'book', author : 'writer', pages : 234, read : true}];
 
+const bookGrid = document.getElementById("book-grid");
+const addBookModal = document.getElementById('add-book-modal');
+const addBookBtn = document.getElementById('add-book-btn');
+const submitBtn = document.getElementById('submit-book-form');
+const closeBtn = document.getElementById('close');
+
+addBookBtn.addEventListener('click', showModal);
+closeBtn.addEventListener('click', closeModal);
+submitBtn.addEventListener('click', submitNewBook);
+
+function getBookInput() {
+    const title = document.getElementById('new-title').value;
+    const author = document.getElementById('new-author').value;
+    const pages = document.getElementById('new-pages').value;
+    const read = document.getElementById('new-read').checked;
+    return new Book(title, author, pages, read);
+}
+
+function submitNewBook(e) {
+    e.preventDefault();
+    const newbook = getBookInput();
+
+    addBookToList(newbook);
+    displayBooks(myBookList);
+    closeModal();
+}
+
 function displayBooks(myBookList) {
+    resetBookGrid();
     for(let book of myBookList) {
         createBookCard(book);
     }
 }
 
-const bookGrid = document.getElementById("book-grid");
+function resetBookGrid() {
+    bookGrid.innerHTML = '';
+}
 
 function createBookCard(book) {
     const bookCard = document.createElement('div');
@@ -42,8 +76,19 @@ function createBookCard(book) {
     bookCard.appendChild(author);
     bookCard.appendChild(pages);
     bookGrid.appendChild(bookCard);
-    console.log(bookCard);
+}
 
+function showModal() {
+    addBookModal.style.display = 'block';
+}
+
+function closeModal() {
+    addBookModal.style.display = 'none';
+    document.getElementById('new-title').value = '';
+    document.getElementById('new-author').value = '';
+    document.getElementById('new-pages').value = '';
+    document.getElementById('new-read').checked = '';
 }
 
 displayBooks(myBookList);
+
